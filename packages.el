acd376069b748c5d72074d3e4ea8be901b365c08
Edvard <elsberg1995@protonmail.com>
2021-03-10 22:58:31 +0100

    updates

--- packages.el
+++ packages.el
@@ -11,7 +11,6 @@
 
 (package! lsp-ui)
 (package! company-lsp)
-(package! rainbow-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
